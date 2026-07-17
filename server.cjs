19090e71d4b30a4c8e7e99c843d61db0dfff0c6a
XULIVSKI01 <xulovski@proton.me>
2026-07-17 21:28:59 +0100

    Atualizar o server.cjs

--- server.cjs
+++ server.cjs
@@ -895,11 +895,7 @@ if (!auth) {
         }
     }
     console.log(`[PROXY TV] Link obtido do portal: ${cleanUrl}`);
-if (cleanUrl && cleanUrl.includes('play_token')) {
-    console.log(`[PROXY TV] play_token detetado, redirecionando...`);
-    return res.redirect(302, cleanUrl);
-}
-execStream(cleanUrl);
+    execStream(cleanUrl);
 } catch (e) {
     console.error("[PROXY] Erro interno no pipe TV:", e.message);
     delete global.pendingTvPromises[streamKey];
